1888cdd493030aaa2e935427f0017db932163c11
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-09-07 15:28:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,4 +23,15 @@ function ternaryCheckCity(city){
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+    return "Bye.";
+  }
+  
 }
