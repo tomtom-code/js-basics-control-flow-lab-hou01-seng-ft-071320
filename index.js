be3471b6126ddf230c2ef099a93b3f443a90ac57
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-09-07 15:02:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function scuberGreetingForFeet(feet) {
     greeting = 'This one is on me!';
   } if (feet > 2000) {
     greeting = 'I will gladly take your thirty bucks.';
-  } elseif (feet >2500) {
+  } else if (feet >2500) {
     greeting = 'No can do.';
-    }
+  }
   return greeting
 }
 
