2e4767377bbb7437bcaaa763c2fe5e3029d83e28
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-09-07 15:29:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  let greeting
+  if (feet <= 400) {
+    return greeting = 'This one is on me!';
+  } else if (1999< feet && feet < 2500) {
+    return greeting = 'I will gladly take your thirty bucks.';
+  } else {
+    return greeting = 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  }else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+    return "Bye.";
+  }
+
 }
